e4ef3ca3633f96af9bef36001104895dad3847c3
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-06 19:22:21 +0200

    Write a code that passes the python pycodestyle checker

---
+++ pycodestyle_compliant.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Contains a simple class code that passes pycodestyle checker
+"""
+
+
+class Person:
+    """ Creates a Person class template """
+    def __init__(self, name):
+        """ Instantiates the object by taking name of object
+            Args:
+                name (str): contains full name of instance object
+        """
+        self.name = name
+
+    def introduce(self):
+        """ Introduces the instance object """
+        print(f"Hello, my name is {self.name}")
+
+
+if __name__ == "__main__":
+    person = Person("Acquah Kingsley Segu")
+    person.introduce()
