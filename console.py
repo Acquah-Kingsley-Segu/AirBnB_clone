7999b39c800f7a4de9ed0c697892095523032d56
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-13 09:57:25 +0200

    Modify content

--- console.py
+++ console.py
@@ -8,28 +8,35 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """ Implements the cmd feature of the console """
 
-
+    ##############################
+    # class attributes overrides #
+    ##############################
     prompt = "(hbnb) "
-    def emptyline(self):
-        """ executes when an empty line is entered """
-        return ""
 
+    ###############################
+    # instance methods ovverrides #
+    ###############################
+    def postloop(self):
+        print("")
+
+    #####################
+    # Commands handlers #
+    #####################
+    def emptyline(self):
+        return False
 
     def do_quit(self, line):
-        """Quit command to exit the program
-        """
         return True
 
     def do_EOF(self, line):
-        """ handle end-of-string charater
-            ie. Ctrl+D
-        """
         return True
-
-    def postloop(self):
-        """ run before cmdloop ends """
-        print("")
-
-
+    
+    ################
+    # command help #
+    ################
+    def help_quit(self):
+        print("Quit command to exit program\n")
+    def help_EOF(self):
+        print("Ctrl + D (EOF) to exit program\n")
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
