b05791bf29495d99f87e35ea6b8ed3f06332063a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-13 09:33:19 +0200

    Add console version 0.0.1

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" The console program implementation """
+
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Implements the cmd feature of the console """
+
+
+    prompt = "(hbnb) "
+    def emptyline(self):
+        """ executes when an empty line is entered """
+        return ""
+
+
+    def do_quit(self, line):
+        """ exits user from interpreter """
+        return True
+
+    def do_EOF(self, line):
+        """ handle end-of-string charater
+            ie. Ctrl+D
+        """
+        return True
+
+    def postloop(self):
+        """ run before cmdloop ends """
+        print()
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
